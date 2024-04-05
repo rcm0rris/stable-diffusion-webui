088a616ce721f6b15b9f4f45765e7128c509a41c
rcm0rris <r_c_morris@yahoo.com>
2024-04-05 09:01:41 +0800

    4/5 working result

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= 
-#set COMMANDLINE_ARGS= --allow-code --xformers --no-half-vae
+REM set COMMANDLINE_ARGS= 
+set COMMANDLINE_ARGS= --xformers --no-half-vae
 git pull
 call webui.bat
